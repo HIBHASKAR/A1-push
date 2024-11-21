c936908e8c023451fe9ad568f1d9942572936eb7
bhask <bhask@LAPTOP-I1MU4LJ5>
2024-11-21 21:51:49 +0530

    do it

---
+++ A1_project_push/src/test/java/contact/To_crate_A_contact.java
@@ -0,0 +1,10 @@
+package contact;
+
+public class To_crate_A_contact {
+
+	public static void main(String[] args) {
+		System.out.println("contact cration done");
+
+	}
+
+}
---
+++ A1_project_push/src/test/java/org/To_crate_org.java
@@ -0,0 +1,10 @@
+package org;
+
+public class To_crate_org {
+
+	public static void main(String[] args) {
+	System.out.println("organization creation done");
+
+	}
+
+}
